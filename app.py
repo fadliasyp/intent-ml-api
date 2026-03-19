cc91a2734de6b8ec87c879ba18b629381babddf6
fadliasyp <riansyahfadli12@gmail.com>
2026-03-19 16:15:22 +0700

    merubah app.py ke .joblib final

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 app = FastAPI()
 
 # load model sekali saat app start
-model = joblib.load("intent_tfidf_logreg2.joblib")
+model = joblib.load("intent_tfidf_logreg_final.joblib")
 
 class PredictRequest(BaseModel):
     question: str
